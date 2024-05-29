b9d3b9f6ea7a1c23a3bcad5fbf7780725bacdb70
sprajapati123 <sandisprajapati52@gmail.com>
2024-05-29 10:13:22 +0545

    add product

--- app/src/main/java/com/example/crud_34b/DashBoardActivity.kt
+++ app/src/main/java/com/example/crud_34b/DashBoardActivity.kt
@@ -2,13 +2,18 @@ package com.example.crud_34b
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.crud_34b.databinding.ActivityDashBoardBinding
+import com.example.crud_34b.model.ProductModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class DashBoardActivity : AppCompatActivity() {
     lateinit var dashBoardBinding: ActivityDashBoardBinding
@@ -28,6 +33,27 @@ class DashBoardActivity : AppCompatActivity() {
                 AddProductActivity::class.java)
             startActivity(intent)
         }
+
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.d("snapshot data ",snapshot.children.toString())
+                for(eachData in snapshot.children){
+
+                    var product = eachData.getValue(ProductModel::class.java)
+                    if(product!=null){
+                        Log.d("data from firebase",product.name)
+                        Log.d("data from firebase",product.description)
+                        Log.d("data from firebase",product.price.toString())
+
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
