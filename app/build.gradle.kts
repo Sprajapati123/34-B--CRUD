2ad81f8b8b130b1bf80836f9ce123f37b4abb113
sprajapati123 <sandisprajapati52@gmail.com>
2024-06-04 07:24:28 +0545

    manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation("com.squareup.picasso:picasso:2.8")
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
--- app/src/main/java/com/example/crud_34b/AddProductActivity.kt
+++ app/src/main/java/com/example/crud_34b/AddProductActivity.kt
@@ -1,25 +1,52 @@
 package com.example.crud_34b
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.crud_34b.databinding.ActivityAddProductBinding
 import com.example.crud_34b.model.ProductModel
 import com.google.firebase.database.FirebaseDatabase
+import com.squareup.picasso.Picasso
 
 class AddProductActivity : AppCompatActivity() {
     var firebaseDatabase : FirebaseDatabase = FirebaseDatabase.getInstance()
     var ref = firebaseDatabase.reference.child("products")
     lateinit var addProductBinding: ActivityAddProductBinding
+
+    lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    var imageUri : Uri? = null
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            activityResultLauncher.launch(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         addProductBinding = ActivityAddProductBinding.inflate(layoutInflater)
         setContentView(addProductBinding.root)
 
+        registerActivityForResult()
         addProductBinding.btnPost.setOnClickListener {
             var name : String = addProductBinding.editTextProductName.text.toString()
             var price : Int = addProductBinding.editTextProductPrice.text.toString().toInt()
@@ -47,5 +74,22 @@ class AddProductActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun registerActivityForResult(){
+        activityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback {result ->
+
+                val resultcode = result.resultCode
+                val imageData = result.data
+                if(resultcode == RESULT_OK && imageData != null){
+                    imageUri = imageData.data
+                    imageUri?.let {
+                        Picasso.get().load(it).into(addProductBinding.imageBrowse)
+                    }
+                }
+
+            })
     }
 }
